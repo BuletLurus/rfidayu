d3a316fdccbb161f2f42e66dc4d13bd3a36ce803
BuletLurus <65167200+BuletLurus@users.noreply.github.com>
2021-02-08 18:42:27 +0700

    Add files via upload

---
+++ rfidayu/topup.php
@@ -0,0 +1,130 @@
+<?php
+    require 'database.php';
+    $id = 0;
+     
+    if ( !empty($_GET['id'])) {
+        $id = $_REQUEST['id'];
+    }
+     
+    if ( !empty($_POST)) {
+        // keep track post values
+        $id = $_POST['id'];
+         
+        // delete data
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "UPDATE FROM table_the_iot_projects  WHERE id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        Database::disconnect();
+         
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<script src="js/bootstrap.min.js"></script>
+		<script src="jquery.min.js"></script>
+		<script>
+			$(document).ready(function(){
+				 $("#getUID").load("UIDContainer.php");
+				setInterval(function() {
+					$("#getUID").load("UIDContainer.php");
+				}, 500);
+			});
+		</script>
+		
+		<style>
+		html {
+			font-family: Arial;
+			display: inline-block;
+			margin: 0px auto;
+		}
+		
+		textarea {
+			resize: none;
+		}
+
+		ul.topnav {
+			list-style-type: none;
+			margin: auto;
+			padding: 0;
+			overflow: hidden;
+			background-color: #4CAF50;
+			width: 70%;
+		}
+
+		ul.topnav li {float: left;}
+
+		ul.topnav li a {
+			display: block;
+			color: white;
+			text-align: center;
+			padding: 14px 16px;
+			text-decoration: none;
+		}
+
+		ul.topnav li a:hover:not(.active) {background-color: #3e8e41;}
+
+		ul.topnav li a.active {background-color: #333;}
+
+		ul.topnav li.right {float: right;}
+
+		@media screen and (max-width: 600px) {
+			ul.topnav li.right, 
+			ul.topnav li {float: none;}
+		}
+		</style>
+		
+		<title>TopUP : NodeMCU V3 ESP8266 / ESP12E with MYSQL Database</title>
+	</head>
+
+    <body>
+
+		<h2 align="center">NodeMCU V3 ESP8266 / ESP12E with MYSQL Database</h2>
+		<ul class="topnav">
+			<li><a href="home.php">Home</a></li>
+			<li><a href="user data.php">User Data</a></li>
+            <li><a href="registration.php">Registration</a></li>
+			<li><a href="read tag.php">Read Tag ID</a></li>
+			<li><a class="active" href="topup.php">Top Up</a></li>
+		</ul>
+
+		<div class="container">
+			<br>
+			<div class="center" style="margin: 0 auto; width:495px; border-style: solid; border-color: #f2f2f2;">
+				<div class="row">
+					<h3 align="center">Top Up Form</h3>
+				</div>
+				<br>
+				<form class="form-horizontal" action="topup.php" method="post" >
+					<div class="control-group">
+						<label class="control-label">ID</label>
+						<div class="controls">
+							<textarea name="id" id="getUID" placeholder="Please Scan your Card / Key Chain to display ID" rows="1" cols="1" required></textarea>
+						</div>
+					</div>
+					
+					<div class="control-group">
+						<label class="control-label">Amount</label>
+						<div class="controls">
+							<input id="div_refresh" name="amount" type="number"  placeholder="" required>
+						</div>
+					</div>
+					
+					
+					
+					<div class="form-actions">
+						<button type="submit" class="btn btn-success">Save</button>
+                    </div>
+				</form>
+				
+			</div>               
+		</div> <!-- /container -->	
+	</body>
+</html>
