da5a8b3db344c43c5a4c99f130b64bea101250ac
BuletLurus <65167200+BuletLurus@users.noreply.github.com>
2021-02-08 18:46:19 +0700

    Add files via upload

--- rfidayu/nodemcu_rfid_iot_projects.sql
+++ rfidayu/nodemcu_rfid_iot_projects.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 20, 2020 at 12:40 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.3
+-- Host: localhost
+-- Generation Time: Feb 08, 2021 at 12:45 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -33,18 +32,20 @@ CREATE TABLE `table_the_iot_projects` (
   `id` varchar(100) NOT NULL,
   `gender` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `mobile` varchar(100) NOT NULL
+  `mobile` varchar(100) NOT NULL,
+  `amount` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `table_the_iot_projects`
 --
 
-INSERT INTO `table_the_iot_projects` (`name`, `id`, `gender`, `email`, `mobile`) VALUES
-('Alsan', '39EAB06D', 'Male', 'mydigitalnepal@gmail.com', '9800998787'),
-('John', '769174F8', 'Male', 'john@email.com', '23456789'),
-('Thvhm,b', '81A3DC79', 'Female', 'jgkhkkmanjil@gmail.com', '45768767564'),
-('The IoT Projects', '866080F8', 'Male', 'ask.theiotprojects@gmail.com', '9800988978');
+INSERT INTO `table_the_iot_projects` (`name`, `id`, `gender`, `email`, `mobile`, `amount`) VALUES
+('Alsan', '39EAB06D', 'Male', 'mydigitalnepal@gmail.com', '9800998787', 60000000),
+('John', '769174F8', 'Male', 'john@email.com', '23456789', 0),
+('Thvhm,b', '81A3DC79', 'Female', 'jgkhkkmanjil@gmail.com', '45768767564', 0),
+('The IoT Projects', '866080F8', 'Male', 'ask.theiotprojects@gmail.com', '9800988978', 0),
+('panji', 'BUL3T666', 'Male', 'dsff', '4455405904', 500000);
 
 --
 -- Indexes for dumped tables
